a5d98735d29e46594d684c7313d8b3d8fe024bec
yosep <yosepalexsander9@gmail.com>
2021-09-08 10:24:11 +0700

    set events contacts

--- client/src/pages/Complain.js
+++ client/src/pages/Complain.js
@@ -1,9 +1,11 @@
 // import hook
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Navbar from '../components/Navbar'
 
 // import components here
+import {Container, Row, Col} from 'react-bootstrap'
+import Contact from '../components/complain/Contact'
 
 // import socket.io-client 
 import {io} from 'socket.io-client'
@@ -11,26 +13,48 @@ import {io} from 'socket.io-client'
 // initial variable outside component
 let socket
 export default function Complain() {
-    // code here
+    const [contact, setContact] = useState({})
+    const [contacts, setContacts] = useState([])
 
     const title = "Complain"
     document.title = 'DumbMerch | ' + title
 
     useEffect(() =>{
         socket = io('http://localhost:5000')
-        // code here
+        loadContact()
 
         return () => {
             socket.disconnect()
         }
     }, [])
 
-    // code here
+    const loadContact = () => {
+        socket.emit("load admin contact")
+
+        socket.on("admin contact", (data) => {
+            console.log(data)
+            const dataContact = {
+                ...data,
+                message: "Click here to start message" 
+            }
+
+            setContacts([dataContact])
+        })
+    }
     
+    const onClickContact = (data) => {
+        setContact(data)
+    }
     return (
         <>
             <Navbar title={title} />
-            {/* code here */}
+            <Container fluid style={{height: '89.5vh'}}>
+                <Row>
+                    <Col md={3} style={{height: '89.5vh'}} className="px-3 border-end border-dark overflow-auto">
+                        <Contact dataContact={contacts} clickContact={onClickContact} contact={contact}/>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -1,11 +1,63 @@
 // import models here
+const {user, chat} = require('../../models')
 
 const socketIo = (io) => {
   io.on('connection', (socket) => {
     console.log('client connect: ', socket.id)
 
-    // code here
+    socket.on("load admin contact", async() => {
+      try {
+        const adminContact = await user.findOne({
+          where: {
+            status: "admin"
+          },
+          attributes: {
+            exclude: ["createdAt", "updatedAt", "password"]
+          }
+        })
 
+        socket.emit("admin contact", adminContact)
+      } catch (error) {
+        console.log(error)
+      }
+    })
+
+    socket.on("load customer contacts", async() => {
+      try {
+        let customerContacts = await user.findAll({
+          include: [{
+            model: chat,
+            as: "recipientMessage",
+            attributes: {
+              exclude: ["createdAt", "updatedAt", "idRecipient", "idSender"]
+            }
+          },
+          {
+            model: chat,
+            as: "senderMessage",
+            attributes: {
+              exclude: ["createdAt", "updatedAt", "idRecipient", "idSender"]
+            }
+          },
+        ],
+          attributes: {
+            exclude: ["createdAt", "updatedAt", "password"]
+          }
+        })
+
+        customerContacts = customerContacts.map(item => ({
+          ...item,
+          profile: {
+            ...item.profile,
+            image: item.profile?.image ? process.env.PATH_FILE + item.profile?.image : null
+          }
+        }))
+
+        socket.emit("customer contacts", customerContacts)
+      } catch (error) {
+        console.log(error)
+      }
+    })
     socket.on("disconnect", () => {
       console.log("client disconnect")
     })
